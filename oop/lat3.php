bf45890986b637cf911b2b7f10e5ebec0efcf011
Adityariman1 <adityariman96@gmail.com>
2021-09-13 22:15:56 -0700

    pelajaran senin

--- oop/lat3.php
+++ oop/lat3.php
@@ -44,25 +44,25 @@ class Aritmatika{
         $this-> bilangan2 = $y;
     }
     public function tambah()
-        {  
+    {  
             return ($this->bilangan1 + $this->bilangan2 );
-        }
+    }
     public function kurang()
-        {  
+    {  
             return ($this->bilangan1 - $this->bilangan2 );
-        }
+    }
     public function kali()
-        {  
+    {  
             return ($this->bilangan1 * $this->bilangan2 );
-        }
+    }
     public function bagi()
-        {  
+    {  
             return ($this->bilangan1 / $this->bilangan2 );
-        }
+    }
     public function modulus()
-        {  
+    {  
             return ($this->bilangan1 % $this->bilangan2 );
-        }
+    }
 }
 $aritmatika1 = new aritmatika($bilangan1,$bilangan2);
 echo "Bilangan 1 : " . $aritmatika1->bilangan1 . "<br>";
@@ -72,4 +72,5 @@ echo "Pengurangan : " . $aritmatika1->kurang()  . "<br>";
 echo "Perkalian : " . $aritmatika1->kali()  . "<br>";
 echo "Pembagian : " . $aritmatika1->bagi()  . "<br>";
 echo "Modulus : " . $aritmatika1->modulus()  . "<br>";
+
 ?>
